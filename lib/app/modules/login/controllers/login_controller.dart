a9a536bc4a1cdf8611200699689aaf23f770987d
mosfeqanik <mosfeqanik01@gmail.com>
2023-01-14 22:28:21 +0600

    connectivity check added

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -13,81 +14,93 @@ class LoginController extends GetxController {
   final passwordController = TextEditingController();
   final auth = FirebaseAuth.instance;
   var isSignUpButtonClick = false.obs;
+
   String? loggedInEmail;
+
   loginFunc() async {
-    if (emailController.text.isEmpty && emailController.text.isEmpty) {
-      Get.snackbar(
-          backgroundColor: Colors.red,
-          "oops",
-          "input data is missing",
-          colorText: Colors.white);
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.none) {
+      Get.snackbar("oops", "No Internet", backgroundColor: Colors.red);
     } else {
-      try {
-        final user = await auth.signInWithEmailAndPassword(
-            email: emailController.text, password: passwordController.text);
-        if (user != null) {
-          if (emailController.text.isNotEmpty &&
-              emailController.text.isNotEmpty) {
-            SharedPreferencesRepository.SetString(
-                SharedPrefKeys.token, "value");
-            loggedInEmail = FirebaseAuth.instance.currentUser?.email;
-            SharedPreferencesRepository.SetString(
-                SharedPrefKeys.userEmail, loggedInEmail!);
-            Get.snackbar(
-                backgroundColor: Colors.green,
-                "Hello",
-                "Welcome",
-                colorText: Colors.white);
-            Get.offNamed(Routes.HOME);
-          } else {
-            Get.snackbar(
-                backgroundColor: Colors.red,
-                "OOPs",
-                "something is wrong",
-                colorText: Colors.white);
+      if (emailController.text.isEmpty && emailController.text.isEmpty) {
+        Get.snackbar(
+            backgroundColor: Colors.red,
+            "oops",
+            "input data is missing",
+            colorText: Colors.white);
+      } else {
+        try {
+          final user = await auth.signInWithEmailAndPassword(
+              email: emailController.text, password: passwordController.text);
+          if (user != null) {
+            if (emailController.text.isNotEmpty &&
+                emailController.text.isNotEmpty) {
+              SharedPreferencesRepository.SetString(
+                  SharedPrefKeys.token, "value");
+              loggedInEmail = FirebaseAuth.instance.currentUser?.email;
+              SharedPreferencesRepository.SetString(
+                  SharedPrefKeys.userEmail, loggedInEmail!);
+              Get.snackbar(
+                  backgroundColor: Colors.green,
+                  "Hello",
+                  "Welcome",
+                  colorText: Colors.white);
+              Get.offNamed(Routes.HOME);
+            } else {
+              Get.snackbar(
+                  backgroundColor: Colors.red,
+                  "OOPs",
+                  "something is wrong",
+                  colorText: Colors.white);
+            }
           }
+        } catch (e) {
+          print("anik--> $e");
         }
-      } catch (e) {
-        print("anik--> $e");
       }
     }
   }
 
   registerFunc() async {
-    if (emailController.text.isEmpty && emailController.text.isEmpty) {
-      Get.snackbar(
-          backgroundColor: Colors.red,
-          "oops",
-          "input data is missing",
-          colorText: Colors.white);
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.none) {
+      Get.snackbar("oops", "No Internet", backgroundColor: Colors.red);
     } else {
-      try {
-        final user = await auth.createUserWithEmailAndPassword(
-            email: emailController.text, password: passwordController.text);
-        if (user != null) {
-          if (emailController.text.isNotEmpty &&
-              emailController.text.isNotEmpty) {
-            SharedPreferencesRepository.SetString(
-                SharedPrefKeys.token, "value");
-            loggedInEmail = FirebaseAuth.instance.currentUser?.email;
-            SharedPreferencesRepository.SetString(
-                SharedPrefKeys.userEmail, loggedInEmail!);
-            Get.snackbar(
-                backgroundColor: Colors.green,
-                "Hello",
-                "Welcome",
-                colorText: Colors.white);
-            Get.offNamed(Routes.HOME);
-          } else {
-            Get.snackbar(
-                backgroundColor: Colors.red,
-                "OOPs",
-                "something is wrong",
-                colorText: Colors.white);
+      if (emailController.text.isEmpty && emailController.text.isEmpty) {
+        Get.snackbar(
+            backgroundColor: Colors.red,
+            "oops",
+            "input data is missing",
+            colorText: Colors.white);
+      } else {
+        try {
+          final user = await auth.createUserWithEmailAndPassword(
+              email: emailController.text, password: passwordController.text);
+          if (user != null) {
+            if (emailController.text.isNotEmpty &&
+                emailController.text.isNotEmpty) {
+              SharedPreferencesRepository.SetString(
+                  SharedPrefKeys.token, "value");
+              loggedInEmail = FirebaseAuth.instance.currentUser?.email;
+              SharedPreferencesRepository.SetString(
+                  SharedPrefKeys.userEmail, loggedInEmail!);
+              Get.snackbar(
+                  backgroundColor: Colors.green,
+                  "Hello",
+                  "Welcome",
+                  colorText: Colors.white);
+              Get.offNamed(Routes.HOME);
+            } else {
+              Get.snackbar(
+                  backgroundColor: Colors.red,
+                  "OOPs",
+                  "something is wrong",
+                  colorText: Colors.white);
+            }
           }
+        } catch (e) {
+          print("anik--> $e");
         }
-      } catch (e) {
-        print("anik--> $e");
       }
     }
   }
