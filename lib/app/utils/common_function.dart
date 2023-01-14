9b83f4a127627d99d2029a9e4dabba3c505bcba6
mosfeqanik <mosfeqanik01@gmail.com>
2023-01-14 22:12:46 +0600

    date formater Function added

--- lib/app/utils/common_function.dart
+++ lib/app/utils/common_function.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 BoxDecoration headingDecoration() {
   return const BoxDecoration(
@@ -7,4 +8,17 @@ BoxDecoration headingDecoration() {
     Colors.blue,
     Colors.white,
   ]));
+}
+
+// dateformatter
+String dateformaterFuncDate({required String inputString}) {
+  final DateTime inputDateTime;
+  if (inputString.isNotEmpty) {
+    inputDateTime = DateTime.parse(inputString);
+  }else{
+    inputDateTime = DateTime.parse("0000-00-00");
+  }
+  final DateFormat formatter = DateFormat('yyyy-MM-dd');
+  final String formatted = formatter.format(inputDateTime);
+  return formatted;
 }
