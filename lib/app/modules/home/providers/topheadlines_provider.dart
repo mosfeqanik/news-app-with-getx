6f9b47a3878ce8923c24b798d25e4b81cb6cdebd
mosfeqanik <mosfeqanik01@gmail.com>
2023-01-14 22:14:44 +0600

    top news headline api hit added

--- lib/app/modules/home/providers/topheadlines_provider.dart
+++ lib/app/modules/home/providers/topheadlines_provider.dart
@@ -1,15 +1,17 @@
-import 'dart:convert';
-
 import 'package:get/get.dart';
 import 'package:news_app_with_getx/app/utils/APIs.dart';
 
-import '../topheadlines_model.dart';
 
 class TopheadlinesProvider extends GetConnect {
 
-  Future<GetTopHeadlines?> getTopheadlines() async {
+  Future<String> getTopHeadlines() async {
     final response = await get(APIs.TopHeadlines);
-    return GetTopHeadlines.fromJson(jsonDecode(response.body));
+    if (response.status.hasError) {
+      return Future.error(response.bodyString!);
+    } else {
+      return response.bodyString!;
+    }
   }
+
 
 }
